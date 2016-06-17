2521b0f18e0ffed829a4a550cced50e03da54fcd
rlukash <babkamen@gmail.com>
2016-06-17 05:12:29 +0300

    Init

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+import java.io.InputStream;
+import java.io.StringWriter;
+
+
+public class Main {
+    public static void main(String[] args) throws UnirestException {
+        InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("issue.json");
+        String body= convertStreamToString(inputStream);
+        System.out.println("Body="+body);
+        HttpResponse<JsonNode> response = Unirest.get("https://XXX.atlassian.net/rest/api/2/issue")
+                .basicAuth("XXX", "XXX")
+               .header("content-type", "application/json")
+               .body(body)
+                .asJson();
+        System.out.println(response.getStatus()+"\t"+response.getStatusText()+"\nBody="+response.getBody().toString());
+    }
+    static String convertStreamToString(java.io.InputStream is) {
+        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
+    }
+}
